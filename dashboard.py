14956e9ddeb83146fb10b64f2e2f9f3c2fdedcf5
Muhammad marcilano <marrcilanoprawira@gmail.com>
2026-04-07 18:33:06 +0700

    Update dashboard.py
    interaktif apps dsh streamlit

--- dashboard.py
+++ dashboard.py
@@ -4,22 +4,25 @@ import seaborn as sns
 import streamlit as st
 sns.set(style='dark')
 
-# set halaman
-st.set_page_config(page_title="Dashboard Penyewaan Sepeda",  layout="wide")
+# header
+st.set_page_config(page_title="Dashboard Penyewaan Sepeda (2011-2012)", layout="wide")
 st.title("Dashboard Analisis Penyewaan Sepeda")
-st.markdown("""Dashboard ini saya kembangkan untuk memberikan wawasan mendalam tentang tren penyewaan sepeda berdasarkan data historis. Sehingga dapat memahami: 
+st.markdown("""
+Dashboard ini dikembangkan untuk memberikan visualiasi dan insight tentang tren penyewaan sepeda berdasarkan data historis (2011-2012). 
+Sehingga dapat melihat langsung : 
 1. Pola Peminjaman Harian by Pelanggan Casual dan Registered
-2. tren penyewaan yang berhubungan dengan cuaca      
-3. Pola/perilaku Pelanggan member (Registed) dan non-member (Casual)
-4. Lebih banyak mana Pelanggan saat ini (Resgistered) atau (Casual) ? """)
+2. fluktuasi tren penyewaan terhadap cuaca & musim      
+3. Pola/perilaku Pelanggan member (Registered) dan non member (Casual)
+4. Proporsi dominasi tipe pelanggan di setiap rentan waktu selama periode 2011-2012.
+""")
 
-# Load main_data
+# load main_data
 @st.cache_data
 def load_data():
     df = pd.read_csv("main_data.csv")
     df['dteday'] = pd.to_datetime(df['dteday'])
     
-    # Mengurutkan kategori musim 
+    # urutan musim
     if 'season' in df.columns:
         urutan_musim = ['Spring', 'Summer', 'Fall', 'Winter']
         df['season'] = pd.Categorical(df['season'], categories=urutan_musim, ordered=True)
@@ -29,33 +32,37 @@ df = load_data()
 
 st.divider() 
 
-# visual pula prminjaman harian berdasarkan berdasarkan tipe pelanggan
-st.header("1. Pola Peminjaman Harian berdasarkan berdasarkan tipe pelanggan")
+# chart visualisasi
+st.header("1. Pola Peminjaman Harian Berdasarkan Tipe Pelanggan")
 
-# 'hr'rata-rata 
 jam_rental = df.groupby('hr')[['casual', 'registered']].mean().reset_index()
-
-# Meleburkan data (melt) agar Seaborn bisa membedakan dua garis warna
 jam_rental_melted = pd.melt(jam_rental, id_vars=['hr'], value_vars=['casual', 'registered'],
-                              var_name='Pelanggan', value_name='Rata-rata Peminjam')
+                            var_name='Pelanggan', value_name='Rata-rata Peminjam')
 
 fig1, ax1 = plt.subplots(figsize=(10, 5))
 sns.lineplot(data=jam_rental_melted, x='hr', y='Rata-rata Peminjam', hue='Pelanggan', 
              palette=['#66b3ff', '#ff9999'], marker='o', linewidth=2, ax=ax1)
 
+# highlight area siang-sore untuk Casual, supaya bisa ambil kesimpulan untuk keputusan proosi membership
+ax1.axvspan(12, 16, color='#ff9999', alpha=0.1)
+
 ax1.set_title('Pola Peminjaman Rata-rata per Jam (Casual dan Registered)', fontsize=14)
 ax1.set_xlabel('Jam (0-23)', fontsize=12)
 ax1.set_ylabel('Rata-rata Jumlah Peminjam', fontsize=12)
-
 ax1.set_xticks(range(0, 24))
 ax1.grid(True, linestyle='--', alpha=0.5) 
-
 st.pyplot(fig1)
+
+st.info("""
+**Insight:** Ternyata para pekerja kantoran dan anak sekolah adalah pelanggan *membership* (Registered) yang menyewa sepeda untuk keperluan berangkat (08:00) dan pulang (17:00) kerja/sekolah. 
+
+**Rekomendasi tindakan bisnis :** Tim marketing dapat menargetkan **promosi membership pada pelanggan casual di rentang siang - sore hari (12:00 - 16:00)**, karena pada jam-jam itulah pelanggan *casual* paling aktif melakukan penyewaan.
+""")
 
 st.divider()
 
-# Visual bahwa musim dan cuaca berpengaruh terhadap penyewaan sepeda
-st.header("2. Dampak Musim dan Cuaca Terhadap Penyewaan")
+# fluktuasi penyewaan terhadap cuaca & musim
+st.header("2. Dampak Musim dan Cuaca terhadap Fluktuasi penyewaan")
 col1, col2 = st.columns(2)
 
 with col1:
@@ -76,16 +83,20 @@ with col2:
     ax2b.set_ylabel("Total Penyewaan")
     st.pyplot(fig2b)
 
+st.success("""
+**Insight:** Grafik menunjukkan puncak keramaian terjadi di musim panas (Summer) dan gugur (Fall). 
+
+**Rekomendasi tindakan bisnis :** Tim operasional dapat melakukan **perawatan (maintenance) sepeda secara keseluruhan pada awal musim semi (Spring) dan dingin (Winter)**. Waktu tersebut adalah momen yang paling pas untuk melakukan perawatan kelayakan agar seluruh armada siap disewa maksimal saat musim panas dan gugur tiba.
+""")
+
 st.divider()
 
-#tren penyewaan sepeda sepanjang hari pada hari kerja dan hari libur
-st.header(" 3. Pola Penyewaan Sepeda Sepanjang Hari (00:00 - 23:00)")
+# pola penyewaan sepanjang hari (hari kerja dan libur)
+st.header(" 3. Pola Penyewaan Sepanjang Hari (Hari Kerja dan Libur)")
 
-# pisahihin hari kerja dan hari libur
 hari_kerja_df = df[df['workingday'].isin(['Ya', 1, 'Hari Kerja'])]
 hari_libur_df = df[df['workingday'].isin(['Tidak', 0, 'Hari Libur/Weekend'])]
 
-# rata-rata penyewaan per jam
 tren_kerja = hari_kerja_df.groupby('hr')['cnt'].mean().reset_index()
 tren_libur = hari_libur_df.groupby('hr')['cnt'].mean().reset_index()
 
@@ -97,9 +108,7 @@ with col3:
     ax3.set_title("Tren 24 Jam (HARI KERJA)", fontsize=14)
     ax3.set_xlabel("Jam")
     ax3.set_ylabel("Rata-rata Penyewaan")
-    
     ax3.set_xticks(range(0, 24))
-    # format 00:00
     ax3.set_xticklabels([f"{i:02d}:00" for i in range(0, 24)], rotation=45)
     ax3.grid(True, linestyle='--', alpha=0.6)
     st.pyplot(fig3)
@@ -110,20 +119,54 @@ with col4:
     ax4.set_title("Tren 24 Jam (HARI LIBUR)", fontsize=14)
     ax4.set_xlabel("Jam")
     ax4.set_ylabel("Rata-rata Penyewaan")
-    
     ax4.set_xticks(range(0, 24))
     ax4.set_xticklabels([f"{i:02d}:00" for i in range(0, 24)], rotation=45)
     ax4.grid(True, linestyle='--', alpha=0.6)
     st.pyplot(fig4)
 
+st.warning("""
+**Insight:** Pola keramaian hari kerja dan hari libur memiliki jam jam ramainya masing masing. Ternyata di hari libur tidak ada lonjakan tajam pelanggan menyewa sepeda.
+
+**Rekomendasi tindakan bisnis:** Karena sudah tahu di jam berapa *peak* dan sepinya permintaan pelanggan, tim operasional dapat mempersiapkan dengan baik ketersediaan kelengkapan dan perapihan pemulangan sepeda di titik kumpul penyewaan *sebelum* jam sibuk tersebut.
+""")
+
 st.divider()
 
-# visualisasi proporsi total pelanggan 
+# proporsi total pelanggan 
 st.header("4. Proporsi Total Pelanggan (Registered dan Casual)")
+st.markdown("Gunakan kalender di bawah ini untuk melihat proporsi pelanggan pada rentang waktu tertentu:")
 
-# total penyewaan masing masing tipe pelanggan
-total_casual = df['casual'].sum()
-total_registered = df['registered'].sum()
+# input tanggal
+col_filter1, col_filter2 = st.columns(2)
+min_date = df["dteday"].min()
+max_date = df["dteday"].max()
+
+with col_filter1:
+    start_date = st.date_input(
+        label='Tanggal Mulai',
+        min_value=min_date,
+        max_value=max_date,
+        value=min_date
+    )
+
+with col_filter2:
+    end_date = st.date_input(
+        label='Tanggal Akhir',
+        min_value=min_date,
+        max_value=max_date,
+        value=max_date
+    )
+
+if start_date > end_date:
+    st.error("Harap pastikan Tanggal Akhir tidak mendahului Tanggal Mulai.")
+    st.stop()
+
+filtered_df = df[(df["dteday"] >= pd.to_datetime(start_date)) & 
+                 (df["dteday"] <= pd.to_datetime(end_date))]
+
+# total pelanggan yang sudah di filter sesuai tanggal
+total_casual = filtered_df['casual'].sum()
+total_registered = filtered_df['registered'].sum()
 
 # pie chart
 fig5, ax5 = plt.subplots(figsize=(8, 6))
@@ -132,21 +175,23 @@ sizes = [total_casual, total_registered]
 colors = ['#B22222', '#728C69']
 explode = (0.05, 0) 
 
-ax5.pie(
-    sizes, 
-    explode=explode, 
-    labels=labels, 
-    colors=colors, 
-    autopct='%1.1f%%', 
-    shadow=True, 
-    startangle=140,
-    textprops={'fontsize': 12}
-)
+if total_casual == 0 and total_registered == 0:
+    st.warning("Tidak ada data pada rentang tanggal yang dipilih.")
+else:
+    ax5.pie(
+        sizes, 
+        explode=explode, 
+        labels=labels, 
+        colors=colors, 
+        autopct='%1.1f%%', 
+        shadow=True, 
+        startangle=140,
+        textprops={'fontsize': 12}
+    )
 
-ax5.set_title("Lebih banyak mana pelanggan saat ini?", fontsize=14, fontweight='bold')
+    ax5.set_title(f"Lebih banyak mana pelanggan saat ini?", fontsize=14, fontweight='bold')
+    st.pyplot(fig5)
 
-st.pyplot(fig5)
-
-st.success(f"Pelanggan **Registered** sebagai pelanggan yang paling banyak dengan total **{total_registered:,}** (**Member**), sedangkan Casual **{total_casual:,}**.")
+    st.success(f"Berdasarkan rentang waktu yang dipilih, pelanggan **Registered (Member)** mendominasi dengan total **{total_registered:,}** penyewaan, sedangkan Casual sebanyak **{total_casual:,}**.")
 
 st.caption("Cillano 2026 - Bike Sharing Analisis")
